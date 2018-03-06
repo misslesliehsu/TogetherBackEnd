c3a2a858a19c6e4dffdf175028bd51be6afb1084
Flatiron School <leslie.hsu@flatironschool.com>
2018-03-05 20:02:44 -0500

    minor fix

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -25,7 +25,8 @@ class Api::V1::IdeasController < ApplicationController
       if @idea.valid?
         @idea.save
         params[:date_suggestions].each do |d|
-          date_suggestion = DateSuggestion.new(idea_id: @idea.id, date: d)
+          debugger
+          date_suggestion = DateSuggestion.new(idea_id: @idea.id, date: d.date, voters: [])
           date_suggestion.save if date_suggestion.valid?
         end
         params[:invitees].each do |i|
