686a459c8f450b478e80ddb65056cdce67225706
Flatiron School <leslie.hsu@flatironschool.com>
2018-04-04 11:16:22 -0400

    minor fix

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,6 +2,7 @@ class Idea < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :invitations, dependent: :destroy #if idea dies, so do invitations
   has_many :invitees, through: :invitations
+  has_many :idea_comments, dependent: :destroy 
 
   has_many :date_suggestions, dependent: :destroy #if idea dies, so do date_suggestions
 
